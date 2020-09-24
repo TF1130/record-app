eb53abad82e271c2a875c0792eecd2437548493e
TF1130 <activejob005@gmail.com>
2020-09-24 14:47:41 +0900

    rspec テストコード

--- spec/factories/records.rb
+++ spec/factories/records.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :record do
-    
+    date { '9/22' }
+    breakfast { '卵かけご飯' }
+    lunch { 'カレーライス' }
+    dinner { 'スパゲッティ' }
+    coffeebreak { 'コーヒー' }
   end
 end
--- spec/models/record_spec.rb
+++ spec/models/record_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Record, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+  describe 'レコーディング機能' do
+    before do
+      @record = FactoryBot.build(:record)
+    end
+
+    it 'breakfastが空でも投稿できる' do
+      @record.breakfast = ''
+      expect(@record).to be_valid
+    end
+    it 'lunchが空でも投稿できる' do
+      @record.lunch = nil
+      expect(@record).to be_valid
+    end
+    it 'dinnerが空でも投稿できる' do
+      @record.dinner = nil
+      expect(@record).to be_valid
+    end
+    it 'breakfastが空でも投稿できる' do
+      @record.coffeebreak = nil
+      expect(@record).to be_valid
+    end
